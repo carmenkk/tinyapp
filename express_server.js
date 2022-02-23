623706eecc628d148f6fd6f470498ac0e039bd58
carmen kuang <carmenkuang87@gmail.com>
2022-02-23 06:40:09 +0000

    logout

--- express_server.js
+++ express_server.js
@@ -108,6 +108,13 @@ app.post("/login", (req, res) => {
  
 });
 
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+  res.clearCookie('username');
+  res.redirect('/urls');
+ 
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
