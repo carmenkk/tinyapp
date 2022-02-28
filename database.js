fc712033283dc19bb28f0ff6d3634277e1a05efb
carmen kuang <carmenkuang87@gmail.com>
2022-02-28 01:49:39 +0000

    move database from express_server

---
+++ database.js
@@ -0,0 +1,29 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
+
+
+const users = {
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "01234"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "56789"
+  }
+};
+
+module.exports = {
+  urlDatabase,
+  users,
+}
